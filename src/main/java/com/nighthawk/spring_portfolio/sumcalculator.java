f02743e1cf498b4a95fb2aea2cf3348ec66ade10
Pranavi <pranukurti@gmail.com>
2022-08-22 17:47:20 -0700

    added sumcalculator file and fixed bug

---
+++ src/main/java/com/nighthawk/spring_portfolio/sumcalculator.java
@@ -0,0 +1,17 @@
+package com.nighthawk.spring_portfolio;
+
+public class sumcalculator {
+  public static void main(String[] args)
+ {
+    // two integer variables with values
+      // and a variable "sum" to store the result
+      int num1 = 5, num2 = 15,sum;
+  
+      //calculating the sum of num1 and num2 and
+      //storing the result in the variable sum
+      sum = num1+num2;
+  
+      //printing the result
+      System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
+    }
+ }
